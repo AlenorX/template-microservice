cd614eac68aa65f03782132ea99f697352ccc740
AlenorX <ilyalepa333@gmail.com>
2026-02-10 12:55:17 +0300

    Updated files

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,11 @@
-#Your instructions should be here.
+FROM python:3.11-alpine
+
+WORKDIR /template-microservice
+
+COPY . .
+
+RUN pip install -r req.txt
+
+EXPOSE 8080
+
+CMD ["uvicorn", "main:app",  "--host 127.0.0.1", "--port 8000"]
--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-#Main code
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+@app.get("/status")
+def status():
+    return {"status": True, "message": "Microservice is alive"}
+
+@app.get("/root")
+def root():
+    return {"message": "Hello, World!"}
